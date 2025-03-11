3252ef63cb07171ad68ba7e3bc8602e0e9ccfcf2
Mohammad-Aghaei <111427824+Mohammad-Aghai@users.noreply.github.com>
2025-03-12 00:42:42 +0330

    Delete todo list with redux directory

--- todo list with redux/js/redux/actionType.js
+++
@@ -1,6 +0,0 @@
-//todoActionType
-export const addTodoType = "ADD_TODO"
-export const deleteTodoType = "DELETE_TODO"
-export const clearAllTodoType = "CLEAR_TODO"
-export const statusTodoType = "STATUS_TODO"
-export const getAllTodoType =  "GETALL_TODO"
